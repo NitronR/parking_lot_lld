897da2b616eeece6d74a28da1465427ec456acf4
Bhanu Pratap Singh <bhanur05@gmail.com>
2023-05-22 23:06:59 +0530

    Test outlines

---
+++ src/test/java/parking_lot/LowestFirstParkingStrategyTests.java
@@ -0,0 +1,12 @@
+package parking_lot;
+
+import org.junit.Test;
+
+public class LowestFirstParkingStrategyTests {
+
+    @Test
+    public void should_returnLowestAvailableParkingSlot_when_getAvailableParkingSlotCalled() {
+
+    }
+
+}
---
+++ src/test/java/parking_lot/ParkingLotSystemTests.java
@@ -0,0 +1,71 @@
+package parking_lot;
+
+import org.junit.Test;
+
+public class ParkingLotSystemTests {
+
+    @Test
+    public void should_haveFreeSlotCountEqualToTotalSlotCount_whenParkingLotCreated() {
+
+    }
+
+    @Test
+    public void should_haveAllFreeSlots_whenParkingLotCreated() {
+
+    }
+
+    @Test
+    public void should_haveNoOccupiedSlots_whenParkingLotCreated() {
+
+    }
+
+    @Test
+    public void should_increaseCarFreeSlotCount_whenCarParked() {
+
+    }
+
+    @Test
+    public void should_increaseBikeFreeSlotCount_whenBikeParked() {
+
+    }
+
+    @Test
+    public void should_throwParkingLotFullException_whenCarParkRequestedForFullParkingLot() {
+
+    }
+
+    @Test
+    public void should_decreaseBikeFreeSlotCount_whenBikeUnparked() {
+
+    }
+
+    @Test
+    public void should_removeFreeCarSlot_whenCarParked() {
+
+    }
+
+    @Test
+    public void should_addFreeCarSlot_whenCarUnparked() {
+
+    }
+
+    @Test
+    public void should_addFreeBikeSlot_whenBikeUnparked() {
+
+    }
+
+    @Test
+    public void should_addOccupiedCarSlot_whenCarParked() {
+
+    }
+
+    @Test
+    public void should_removeOccupiedCarSlot_whenCarUnparked() {
+
+    }
+
+    @Test
+    public void should_removeOccupiedBikeSlot_whenBikeUnparked() {
+
+    }
+}
